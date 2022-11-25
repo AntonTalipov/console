4666ebf85354b504d743fb2d8171ad4a67df5182
«Anton» <ant.talipov@gmail.com>
2022-11-25 08:33:22 +0300

    commit summa

--- first/summa/Program.cs
+++ first/summa/Program.cs
@@ -1,4 +1,6 @@
-﻿int number1 = 7;
-int number2 = 13;
+﻿int number1 = new Random().Next(1,10); // от 1 до 10 рандомное
+Console.WriteLine(number1);
+int number2 = new Random().Next(10,20);
+Console.WriteLine(number2);
 int sum = number1 + number2;
 Console.WriteLine(sum);
