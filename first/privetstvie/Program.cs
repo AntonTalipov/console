6dc8fff0ad08230261eb45b133a1ad7c700f5227
«Anton» <ant.talipov@gmail.com>
2022-11-25 08:55:03 +0300

    condition if

---
+++ first/privetstvie/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Type your name!");
+string name = Console.ReadLine();
+if (name.ToLower() == "jon")
+{Console.WriteLine("Hello my dear friend");}
+else {Console.WriteLine("Hello");}
