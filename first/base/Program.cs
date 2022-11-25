a1150ef0923ef747f6c8f2a7c0a194d611088c7f
«Anton» <ant.talipov@gmail.com>
2022-11-25 09:19:08 +0300

    base

---
+++ first/base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 4;
+int b = 7;
+int c = 2;
+int d = 5;
+int e = 6;
+
+int max = a;
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+Console.WriteLine(max);
