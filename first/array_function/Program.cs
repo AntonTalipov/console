470230adccae5009a3ae0212e7576acf1d4f36ac
«Anton» <ant.talipov@gmail.com>
2022-12-01 10:54:25 +0300

    array function

---
+++ first/array_function/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Было: 6 8 3 2 1 4 5 7
+Стало: 1 8 3 2 6 4 5 7 */
+
+int [] arr = {2, 4, 3, 9, 5, 3, 8, 7};
+void PrintArray(int [] array) 
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write( $"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int [] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+      int minPosition = i;
+      for (int j = i + 1; j < array.Length; j++)
+      {
+        if (array[j] < array[minPosition])
+        {
+          minPosition = j;  
+        }
+      }
+
+
+      int temporary = array [i];
+      array[i] = array[minPosition];  
+      array[minPosition]=temporary;   
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
